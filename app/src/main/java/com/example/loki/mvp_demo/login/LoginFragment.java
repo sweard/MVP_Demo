03e9630f21358498d4ba2adba9e39c1498c5bc78
loki <sweord@foxmail.com>
2016-11-28 18:03:03 +0800

    wtf

--- app/src/main/java/com/example/loki/mvp_demo/login/LoginFragment.java
+++ app/src/main/java/com/example/loki/mvp_demo/login/LoginFragment.java
@@ -6,14 +6,39 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
 
 import com.example.loki.mvp_demo.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by loki on 16-11-28.
  */
 
 public class LoginFragment extends Fragment {
+
+
+    @Bind(R.id.login_progress)
+    ProgressBar loginProgress;
+
+    @Bind(R.id.email)
+    AutoCompleteTextView email;
+
+    @Bind(R.id.password)
+    EditText password;
+
+    @Bind(R.id.email_sign_in_button)
+    Button emailSignInButton;
+
+
+    @Bind(R.id.login_form)
+    ScrollView loginForm;
 
     public static LoginFragment newInstance(String id) {
         Bundle args = new Bundle();
@@ -32,7 +57,14 @@ public class LoginFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_login,container,false);
+        View root = inflater.inflate(R.layout.fragment_login, container, false);
+        ButterKnife.bind(this, root);
         return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
     }
 }
--- app/src/main/java/com/example/loki/mvp_demo/login/LoginPresenter.java
+++ app/src/main/java/com/example/loki/mvp_demo/login/LoginPresenter.java
@@ -1,8 +1,141 @@
 package com.example.loki.mvp_demo.login;
 
+import android.content.Context;
+import android.os.AsyncTask;
+import android.text.TextUtils;
+
+import com.example.loki.mvp_demo.R;
+
 /**
  * Created by loki on 16-11-28.
  */
 
-public interface LoginPresenter {
+public class LoginPresenter implements LoginContract.Presenter{
+
+    /**
+     * A dummy authentication store containing known user names and passwords.
+     * TODO: remove after connecting to a real authentication system.
+     */
+    private static final String[] DUMMY_CREDENTIALS = new String[]{
+            "foo@example.com:hello", "bar@example.com:world"
+    };
+
+    private final UserRepository mUserRepository;
+    private final LoginContract.View mLoginView;
+    private final Context mContext;
+
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+    private UserLoginTask mAuthTask = null;
+
+    @Override
+    public void login() {
+
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    private void attemptLogin() {
+        if (mAuthTask != null) {
+            return;
+        }
+
+        // Reset errors.
+        mLoginView.setEmailError(null);
+        mLoginView.setPasswordError(null);
+
+        // Store values at the time of the login attempt.
+        String email = mLoginView.getUserEmail();
+        String password = mLoginView.getPassword();
+
+        boolean cancel = false;
+
+        // Check for a valid mEmailView address & password.
+        if (TextUtils.isEmpty(email)) {
+            cancel = mLoginView.setEmailError(mContext.getString(R.string.error_field_required));
+        } else if (!mLoginView.isEmailValid(email)) {
+            cancel = mLoginView.setEmailError(mContext.getString(R.string.error_invalid_email));
+        } else if (TextUtils.isEmpty(password)) {
+            cancel = mLoginView.setPasswordError(mContext.getString(R.string.error_field_required));
+        } else if (!mLoginView.isPasswordValid(password)) {
+            cancel = mLoginView.setPasswordError(mContext.getString(R.string.error_invalid_password));
+        }
+
+        if (!cancel) {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            mLoginView.showLoginProgress(true);
+            mAuthTask = new UserLoginTask(email, password);
+            mAuthTask.execute((Void) null);
+        }
+    }
+
+    /**
+     * Represents an asynchronous login/registration task used to authenticate
+     * the user.
+     */
+    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String mEmail;
+        private final String mPassword;
+
+        UserLoginTask(String email, String password) {
+            mEmail = email;
+            mPassword = password;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // TODO: attempt authentication against a network service.
+
+            try {
+                // Simulate network access.
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                return false;
+            }
+
+            for (String credential : DUMMY_CREDENTIALS) {
+                String[] pieces = credential.split(":");
+                if (pieces[0].equals(mEmail)) {
+                    // Account exists, return true if the mPasswordView matches.
+                    return pieces[1].equals(mPassword);
+                }
+            }
+
+            // TODO: register the new account here.
+            return false;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mAuthTask = null;
+            mLoginView.showLoginProgress(false);
+
+            if (success) {
+                mLoginView.toMainAct();
+                if (mUserRepository != null) {
+                    mUserRepository.saveUserInfo(new User(mLoginView.getUserEmail(),
+                            mLoginView.getPassword()));
+                }
+            } else {
+                mLoginView.showFailedError();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mAuthTask = null;
+            mLoginView.showLoginProgress(false);
+        }
+    }
 }
